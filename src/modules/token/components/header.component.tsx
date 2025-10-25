e7738d8091cfffc141c131828a643c8fb94d6800
Ilya Yeresko <ereskoilal@gmail.com>
2025-10-25 15:22:23 +0300

    Initial commit - fresh start

---
+++ src/modules/token/components/header.component.tsx
@@ -0,0 +1,66 @@
+import React, {
+	useState,
+} from 'react'
+import {
+	cx,
+} from '@emotion/css'
+import {
+	Telegram, Twitter, PumpFun,
+} from '../../../assets/icons'
+import {
+	Button, ButtonType, Color, Size,
+} from '../../../shared/components'
+import * as styles from './header.style'
+
+export const Header: React.FC = () => {
+	const [isOpen, setIsOpen,] = useState(false,)
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.header}>
+				<p className={styles.title}>PumpPola</p>
+
+				<div
+					className={styles.burger}
+					onClick={():void => {
+						setIsOpen(!isOpen,)
+					}}
+				>
+					{isOpen ?
+						'✕' :
+						'☰'}
+				</div>
+				<div
+					className={cx(styles.linkBlock, isOpen && styles.linkBlockActive,)}
+				>
+					<a className={styles.links} href='#home'>Home</a>
+					<a className={styles.links} href='#discovery'>Discovery</a>
+					<a className={styles.links} href='#live'>Live</a>
+					<a className={styles.links} href='#partners'>Partners</a>
+				</div>
+
+				<div className={styles.iconBlock}>
+					<a target='_blank'
+						rel='noopener noreferrer'
+						href='#'
+						className={styles.links}>
+						<Telegram width={20} height={20} /></a>
+					<a target='_blank'
+						rel='noopener noreferrer'
+						href='#'
+						className={styles.links}>
+						<Twitter width={20} height={20} /></a>
+					<a className={styles.links}><PumpFun width={20} height={20} /></a>
+					<Button
+						additionalProps={{
+							btnType: ButtonType.TEXT,
+							text:    'Buy',
+							size:    Size.SMALL,
+							color:   Color.MAIN_COLOR,
+						}}
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
---
+++ src/modules/token/token.component.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+	Header,
+} from './components/header.component'
+
+import * as styles from './token.style'
+
+export const App: React.FC = () => {
+	return (
+		<div>
+			<Header />
+			<div className={styles.body}>
+			</div>
+		</div>
+	)
+}
+
+export default App
