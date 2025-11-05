30b9295c2fd6fe6ee9edd536c8a40991fc57d083
Ilya Yeresko <ereskoilal@gmail.com>
2025-11-05 17:40:01 +0200

    feat:basic header

--- backend/src/books/dto/create-book.dto.ts
+++
@@ -1,9 +0,0 @@
-// src/books/dto/create-book.dto.ts
-export class CreateBookDto {
-  title: string;
-  author: string;
-  year: number;
-  cost: number;
-  describe: string;
-  coverImage?: string;
-}
--- src/modules/token/components/header.component.tsx
+++ src/modules/token/components/header.component.tsx
@@ -1,66 +1,94 @@
 import React, {
 	useState,
 } from 'react'
+import './header.css'
+
 import {
-	cx,
-} from '@emotion/css'
+	FiSearch,
+} from 'react-icons/fi'
 import {
-	Telegram, Twitter, PumpFun,
-} from '../../../assets/icons'
+	FaRegUser,
+} from 'react-icons/fa6'
 import {
-	Button, ButtonType, Color, Size,
-} from '../../../shared/components'
-import * as styles from './header.style'
+	FiHeart,
+} from 'react-icons/fi'
+
+import {
+	Link,
+} from 'react-router-dom'
 
 export const Header: React.FC = () => {
-	const [isOpen, setIsOpen,] = useState(false,)
-
+	const scrollToTop = ():void => {
+		window.scrollTo({
+			top:      0,
+			behavior: 'smooth',
+		},)
+	}
 	return (
-		<div className={styles.container}>
-			<div className={styles.header}>
-				<p className={styles.title}>PumpPola</p>
+		<>
+			<nav className='navBar'>
+				<div className='logoLinkContainer'>
+					<div className='logoContainer'>
+						<Link to='/' onClick={scrollToTop}>
+							<h2>books store</h2>
+						</Link>
+					</div>
+					<div className='linkContainer'>
+						<ul>
+							<li>
+								<Link to='/' onClick={scrollToTop}>
+                  HOME
+								</Link>
+							</li>
+							<li>
+								<Link to='/shop' onClick={scrollToTop}>
+                  SHOP
+								</Link>
+							</li>
+							<li>
+								<Link to='/blog' onClick={scrollToTop}>
+                  BLOG
+								</Link>
+							</li>
+							<li>
+								<Link to='/about' onClick={scrollToTop}>
+                  ABOUT
+								</Link>
+							</li>
+							<li>
+								<Link to='/contact' onClick={scrollToTop}>
+                  CONTACT
+								</Link>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div className='iconContainer'>
+					<FiSearch size={22} onClick={scrollToTop} />
+					<Link to='/loginSignUp' onClick={scrollToTop}>
+						<FaRegUser size={22} />
+					</Link>
+					<Link to='/cart' onClick={scrollToTop}>
+						{/* <Badge
+							badgeContent={cart.items.length === 0 ?
+								'0' :
+								cart.items.length}
+							color='primary'
+							anchorOrigin={{
+								vertical:   'bottom',
+								horizontal: 'right',
+							}}
+						>
+							<RiShoppingBagLine size={22} />
+						</Badge>*/}
+					</Link>
+					<FiHeart size={22} onClick={scrollToTop} />
 
-				<div
-					className={styles.burger}
-					onClick={():void => {
-						setIsOpen(!isOpen,)
-					}}
-				>
-					{isOpen ?
-						'✕' :
-						'☰'}
 				</div>
-				<div
-					className={cx(styles.linkBlock, isOpen && styles.linkBlockActive,)}
-				>
-					<a className={styles.links} href='#home'>Home</a>
-					<a className={styles.links} href='#discovery'>Discovery</a>
-					<a className={styles.links} href='#live'>Live</a>
-					<a className={styles.links} href='#partners'>Partners</a>
-				</div>
-
-				<div className={styles.iconBlock}>
-					<a target='_blank'
-						rel='noopener noreferrer'
-						href='#'
-						className={styles.links}>
-						<Telegram width={20} height={20} /></a>
-					<a target='_blank'
-						rel='noopener noreferrer'
-						href='#'
-						className={styles.links}>
-						<Twitter width={20} height={20} /></a>
-					<a className={styles.links}><PumpFun width={20} height={20} /></a>
-					<Button
-						additionalProps={{
-							btnType: ButtonType.TEXT,
-							text:    'Buy',
-							size:    Size.SMALL,
-							color:   Color.MAIN_COLOR,
-						}}
-					/>
-				</div>
-			</div>
-		</div>
+			</nav>
+		</>
 	)
 }
+
+export default Header
+
