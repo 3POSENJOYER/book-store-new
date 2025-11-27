07365f0fe9027b3e8116c6da4af258cca720f555
1chuny <145117987+1chuny@users.noreply.github.com>
2025-11-27 11:24:34 +0200

    feat: connected MongoDB to authmodule

--- book-store-backend/src/app.module.ts
+++ book-store-backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
@@ -7,7 +8,15 @@ import { AuthModule } from './auth/auth.module';
 import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [ProductsModule, AuthModule, CartModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://koufeed:A132465c@cluster1.tti5h.mongodb.net/bookstore?retryWrites=true&w=majority',
+    ),
+
+    ProductsModule,
+    AuthModule,
+    CartModule,
+  ],
   controllers: [AppController, ServerDataController],
   providers: [AppService],
 })
--- book-store-backend/src/auth/auth.service.ts
+++ book-store-backend/src/auth/auth.service.ts
@@ -3,52 +3,54 @@ import {
   ConflictException,
   UnauthorizedException,
 } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-  password: string;
-}
-
 @Injectable()
 export class AuthService {
-  private users: User[] = [];
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   // --- Реєстрація ---
-  register(registerDto: RegisterDto) {
-    const userExists = this.users.find(
-      (user) => user.email === registerDto.email,
-    );
-    if (userExists) {
-      throw new ConflictException('Користувач з таким email вже існує');
+  async register(registerDto: RegisterDto) {
+    const existingUser = await this.userModel.findOne({
+      email: registerDto.email,
+    });
+
+    if (existingUser) {
+      throw new ConflictException('User with this email already exists');
     }
 
-    const newUser: User = {
-      id: Date.now(),
-      name: registerDto.name,
-      email: registerDto.email,
-      password: registerDto.password,
+    const createdUser = new this.userModel(registerDto);
+    const savedUser = await createdUser.save();
+
+    return {
+      message: 'User registered successfully',
+      user: {
+        id: savedUser._id,
+        name: savedUser.name,
+        email: savedUser.email,
+      },
     };
-
-    this.users.push(newUser);
-
-    return { message: 'User registered successfully', user: newUser };
   }
 
   // --- Логін ---
-  login(loginDto: LoginDto) {
-    const user = this.users.find((u) => u.email === loginDto.email);
+  async login(loginDto: LoginDto) {
+    const user = await this.userModel.findOne({ email: loginDto.email });
 
     if (!user || user.password !== loginDto.password) {
-      throw new UnauthorizedException('Невірний email або пароль');
+      throw new UnauthorizedException('Invalid email or password');
     }
 
     return {
       message: 'Login successful',
-      user: { id: user.id, name: user.name, email: user.email },
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
     };
   }
 }
