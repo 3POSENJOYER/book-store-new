0196122021ba995e79bdbd1e957625215ad3d1bc
1chuny <145117987+1chuny@users.noreply.github.com>
2025-12-07 17:04:01 +0200

    Fix lint errors

--- src/modules/token/token.component.tsx
+++ src/modules/token/token.component.tsx
@@ -1,40 +1,50 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Header } from "./components/header.component";
-import { ProductDeatlis } from "./components/products/productDetails";
-import { Products } from "./components/products/products";
-import Authentication from "../../pages/Authentication";
-import Payment from "../../pages/Payment";
-import ShopDetails from "./components/Shop/ShopDetails/ShopDetails";
-import ResetPass from "./components/Authentication/Reset/ResetPass";
-import AddBook from "./components/AddBook/addBook";
-import Shop from "../../pages/Shop";
-import Blog from "./components/social/blog";
-import About from "./components/social/about";
-import Contact from "./components/social/contact";
-import { Main } from "./main";
+import React from 'react'
+import {
+	BrowserRouter, Route, Routes,
+} from 'react-router-dom'
+import {
+	Header,
+} from './components/header.component'
+import {
+	ProductDeatlis,
+} from './components/products/productDetails'
+import {
+	Products,
+} from './components/products/products'
+import Authentication from '../../pages/Authentication'
+import Payment from '../../pages/Payment'
+import ShopDetails from './components/Shop/ShopDetails/ShopDetails'
+import ResetPass from './components/Authentication/Reset/ResetPass'
+import AddBook from './components/AddBook/addBook'
+import Shop from '../../pages/Shop'
+import Blog from './components/social/blog'
+import About from './components/social/about'
+import Contact from './components/social/contact'
+import {
+	Main,
+} from './main'
 
 export const App: React.FC = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/loginSignUp" element={<Authentication />} />
-          <Route path="/resetPassword" element={<ResetPass />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/cart" element={<Payment />} />
-          <Route path="/AddBook" element={<AddBook />} />
-          <Route path="/product/:id" element={<ProductDeatlis />} />{" "}
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-};
+	return (
+		<div>
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path='/' element={<Main />} />
+					<Route path='/products' element={<Products />} />
+					<Route path='/loginSignUp' element={<Authentication />} />
+					<Route path='/resetPassword' element={<ResetPass />} />
+					<Route path='/blog' element={<Blog />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/shop' element={<Shop />} />
+					<Route path='/contact' element={<Contact />} />
+					<Route path='/cart' element={<Payment />} />
+					<Route path='/AddBook' element={<AddBook />} />
+					<Route path='/product/:id' element={<ProductDeatlis />} />{' '}
+				</Routes>
+			</BrowserRouter>
+		</div>
+	)
+}
 
-export default App;
+export default App
