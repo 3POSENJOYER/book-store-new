0ff57f5176c5e9ca46730325ebeaeefba44771ec
1chuny <145117987+1chuny@users.noreply.github.com>
2025-12-07 16:45:15 +0200

    feat: added git-hook

--- src/modules/token/components/Authentication/Dashboard/Dashboard.tsx
+++ src/modules/token/components/Authentication/Dashboard/Dashboard.tsx
@@ -1,36 +1,42 @@
-import {
-	error,
-} from 'console'
-import React , {
-	useState,useEffect,
-} from 'react'
+import React, { useState, useEffect } from "react";
 
-const Dashboard:React.FC = () => {
-	const [user ,setUser,] = useState(null,)
-	const token = localStorage.getItem('token',)
-	useEffect(() => {
-		const fetchUser = async():Promise<void> => {
-			const res = await fetch('http://localhost:5000/api/user/me',{
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-			},)
-			const data = await res.json()
-			setUser(data,)
-		}
-		fetchUser()
-	},[token,],)
-
-	return (
-		<div className='dashboard'>
-			{user.name ?
-				<div className='user'>
-					{user.name}
-				</div> :
-				<p>not found</p>}
-
-		</div>
-	)
+interface IUser {
+  name: string;
 }
 
-export default Dashboard
+const Dashboard: React.FC = () => {
+  const [user, setUser] = useState<IUser | null>(null);
+
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    const fetchUser = async (): Promise<void> => {
+      try {
+        const res = await fetch("http://localhost:5000/api/user/me", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (res.ok) {
+          const data = await res.json();
+          setUser(data);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    if (token) {
+      fetchUser();
+    }
+  }, [token]);
+
+  return (
+    <div className="dashboard">
+      {user?.name ? <div className="user">{user.name}</div> : <p>not found</p>}
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,45 +1,43 @@
-import React, {
-	useState,
-} from 'react'
-import Filter from '../modules/token/components/Shop/Filters/Filter'
-import '../modules/token/components/Shop/Filters/Filter.css'
-import ShopDetails from '../modules/token/components/Shop/ShopDetails/ShopDetails'
-import StoreData from '../modules/token/components/products/data'
+import React, { useState } from "react";
+import Filter from "../modules/token/components/Shop/Filters/Filter";
+import "../modules/token/components/Shop/Filters/Filter.css";
+import ShopDetails from "../modules/token/components/Shop/ShopDetails/ShopDetails";
+import StoreData from "../modules/token/components/products/data";
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 interface Product {
-	productID: number
-	productName: string
-	produtDescription: string
-	frontImg?: string
-	backImg?: string
-	productPrice: number
-	productReviews: string
+  productID: number;
+  productName: string;
+  produtDescription: string;
+  frontImg?: string;
+  backImg?: string;
+  productPrice: number;
+  productReviews: string;
 }
 
 const Shop: React.FC = () => {
-	const [filtered, setFiltered,] = useState<Array<Product>>(StoreData,)
+  const [filtered, setFiltered] = useState<Array<Product>>(StoreData);
 
-	const prices = StoreData.map((p,) => {
-		return p.productPrice
-	},)
-	const min = Math.min(...prices,)
-	const max = Math.max(...prices,)
+  const prices = StoreData.map((p) => {
+    return p.productPrice;
+  });
+  const min = Math.min(...prices);
+  const max = Math.max(...prices);
 
-	const [value1, setValue1,] = useState<Array<number>>([min, max,],)
+  const [value1, setValue1] = useState<Array<number>>([min, max]);
 
-	return (
-		<div className='container'>
-			<Filter
-				setFiltered={setFiltered}
-				value1={value1}
-				setValue1={setValue1}
-				min={min}
-				max={max}
-			/>
-			<ShopDetails value1={value1} filtered={filtered} />
-		</div>
-	)
-}
+  return (
+    <div className="container">
+      <Filter
+        setFiltered={setFiltered}
+        value1={value1}
+        setValue1={setValue1}
+        min={min}
+        max={max}
+      />
+      <ShopDetails value1={value1 as [number, number]} filtered={filtered} />
+    </div>
+  );
+};
 
-export default Shop
+export default Shop;
